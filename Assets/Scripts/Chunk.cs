c2fdee577ae4187e74e83e5a51d2c2836a9173d7
clbrunet <brunet.clement5@gmail.com>
2023-03-23 15:46:40 +0100

    feat: color the spheres using simplex noise

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Unity.Mathematics;
 
 public class Chunk : MonoBehaviour
 {
     private const uint DIMENSION = 8;
+    private const float NOISE_SCALE = 1.5f;
 
     private void Start()
     {
@@ -24,6 +26,9 @@ public class Chunk : MonoBehaviour
                     sphere.transform.parent = transform;
                     sphere.transform.localPosition = new(x, y, z);
                     sphere.transform.localScale = 0.3f * Vector3.one;
+                    float3 point = NOISE_SCALE / (float)DIMENSION * (float3)sphere.transform.position;
+                    float value = noise.snoise(point);
+                    sphere.GetComponent<MeshRenderer>().material.color = new Color(value, value, value);
                 }
             }
         }
