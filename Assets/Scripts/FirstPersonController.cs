d6e60eaf7db87a60ac865b323742ba216b865cc2
clbrunet <brunet.clement5@gmail.com>
2023-03-27 16:38:31 +0200

    feat: add first person camera rotations

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -5,13 +5,56 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 8;
+    private float speed = 12f;
+    [SerializeField]
+    private float sensitivity = 1f;
+    [SerializeField]
+    private float pitchMin = -89f;
+    [SerializeField]
+    private float pitchMax = 89f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+    private void LateUpdate()
     {
         Vector3 inputDirection = GetInputDirection();
         float speedMultiplier = GetSpeedMultiplier();
         transform.Translate(speedMultiplier * speed * Time.deltaTime * inputDirection);
+
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            return;
+        }
+        Vector2 mouseInput = GetMouseInput();
+        Vector3 eulerAngles = transform.eulerAngles;
+        eulerAngles.y += sensitivity * mouseInput.x;
+        if (eulerAngles.x > 180f)
+        {
+            eulerAngles.x = Mathf.Clamp(eulerAngles.x - 360f - sensitivity * mouseInput.y, pitchMin, pitchMax);
+        }
+        else
+        {
+            eulerAngles.x = Mathf.Clamp(eulerAngles.x - sensitivity * mouseInput.y, pitchMin, pitchMax);
+        }
+        transform.eulerAngles = eulerAngles;
     }
 
     private Vector3 GetInputDirection()
@@ -56,5 +99,11 @@ public class FirstPersonController : MonoBehaviour
             speedMultiplier /= 2f;
         }
         return speedMultiplier;
+    }
+
+    private Vector2 GetMouseInput()
+    {
+        Vector2 input = new(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        return input;
     }
 }
